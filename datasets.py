244ec9ec09122062c8f4f989ff968b7c9aef8356
viyx <v.shumkov.v.shumkov@gmail.com>
2020-11-13 14:56:54 +0000

    add lightweight datasets, run on 8 batch size

--- datasets.py
+++ datasets.py
@@ -11,45 +11,106 @@ from functools import partial
 from copy import deepcopy
 
 
-class ColorPermutation():
-    # x, y, x_test, y_test = data
+# class ColorPermutation():
+#     # x, y, x_test, y_test = data
 
-    def __init__(self, max_permutations=1000, max_colors=10, data=None):
-        self.max_permutations = max_permutations
+#     def __init__(self, max_permutations=1000, max_colors=10, data=None):
+#         self.max_permutations = max_permutations
+#         self.max_colors = max_colors
+#         self.name = 'color'
+
+#         if(data is not None):
+#             u_colors = set.union(*[set(np.concatenate(i, axis=None)) for i in data])
+#             colors_to_idx = np.zeros(self.max_colors, dtype=np.long)
+#             colors_to_idx[list(u_colors)] = range(len(u_colors))
+#             self.norms = [[colors_to_idx[ep] for ep in d] for d in data]
+#             perms = np.array(list(itertools.permutations(range(self.max_colors), len(u_colors))))
+#             ind = np.random.choice(len(perms), size=min(self.max_permutations, len(perms)), replace=False)
+#             self.permutations = perms[ind]
+
+#     @classmethod 
+#     def from_data(cls, max_permutations, max_colors, data):
+#         return ColorPermutation(max_permutations, max_colors, data)
+
+#     def __len__(self):
+#         return len(self.permutations)
+
+#     def __getitem__(self, id):
+#         permutation = self.permutations[id]
+#         permuted_data = tuple([[permutation[i] for i in d] for d in self.norms])
+#         return permuted_data
+
+
+
+
+from math import factorial
+import functools
+from itertools import permutations
+class LightWeightColorPermutation():
+    # TODO make shuffling for permutation when limit exists
+    # TODO now we take first #limit permutations, it shifts color distribution to first colors (0,1,2,3..)
+    def __init__(self, limit=1000, max_colors=10):
+        self.limit = limit
         self.max_colors = max_colors
-        self.name = 'color'
 
-        if(data is not None):
-            u_colors = set.union(*[set(np.concatenate(i, axis=None)) for i in data])
-            colors_to_idx = np.zeros(self.max_colors, dtype=np.long)
-            colors_to_idx[list(u_colors)] = range(len(u_colors))
-            self.norms = [[colors_to_idx[ep] for ep in d] for d in data]
-            perms = np.array(list(itertools.permutations(range(self.max_colors), len(u_colors))))
-            ind = np.random.choice(len(perms), size=min(self.max_permutations, len(perms)), replace=False)
-            self.permutations = perms[ind]
+    @functools.lru_cache(10)
+    def P(self, n, r):
+        "Return permutation counts. Order matters."
+        return factorial(n) // factorial(n - r)
 
-    @classmethod 
-    def from_data(cls, max_permutations, max_colors, data):
-        return ColorPermutation(max_permutations, max_colors, data)
+    def count(self, data):
+        """Number of permutations per one task.
+        Find #unique colors in task and then count all permutations. P(n,r).
+        Method should be fast."""
 
-    def __len__(self):
-        return len(self.permutations)
+        u_colors = set.union(*[set(np.concatenate(i, axis=None)) for i in data])
+        c = self.P(self.max_colors, self.max_colors - len(u_colors))
+        return min(self.limit, c)
 
-    def __getitem__(self, id):
-        permutation = self.permutations[id]
-        permuted_data = tuple([[permutation[i] for i in d] for d in self.norms])
-        return permuted_data
+    # @classmethod
+    # def _get_permutation(cls, n, r, idx):
+    #     "Fast equivalent of `list(itertools.permutations(range(n), r))[id]`"
+    #     digits = []
 
+    #     for i in range(r):
+    #         c = cls.P(n - i, r - i)
+    #         q, _ = divmod(c, n - i)
+    #         q1, r1 = divmod(idx, q)
+    #         shift = len(list(filter(lambda x: x <= q1, digits)))
+    #         digits.append(q1 + shift)
+    #         idx = r1
 
-def aug_task(task, arc_ds, max_permutations, max_colors):
-    data = arc_ds[task]
-    inst = ColorPermutation.from_data(max_permutations, max_colors, data)
-    return inst
+    #     return tuple(digits)
+    #     # return int(''.join(map(str, digits)))
 
+    @functools.lru_cache(10)
+    def _get_permutations(self, n, r):
+        return list(permutations(range(n), r))
 
-class ARCDataset:
-    def __init__(self, tasks=None, augs=None, data_folder='./data'):
-        if(tasks is None):
+    def transform_data(self, idx, data):
+        """Get permutated data.
+        Find unique colors in data, then estimate permutation for them
+        and replace data with the permutation.
+
+        """
+        assert idx <= self.limit, "Id should be less or equal then limit. Check caller function."
+        u_colors = set.union(*[set(np.concatenate(i, axis=None)) for i in data])
+        u_colors = np.array(list(u_colors))
+
+        # tuple like (1,5,6)
+        p = self._get_permutations(self.max_colors, len(u_colors))[idx]
+
+        # replace unique colors with permuted ones
+        m = np.zeros(self.max_colors, dtype=np.long)
+        m[u_colors] = p
+
+        # replace all data
+        p_data = tuple([[m[ep] for ep in d] for d in data])
+        return p_data
+
+class LightARCDataset:
+    def __init__(self, tasks=None, transforms=None, data_folder='./data'):
+        if(tasks is None): # load all tasks
             train_tasks = glob.glob(data_folder + '/training/*.json')
             eval_tasks = glob.glob(data_folder + '/evaluation/*.json')
             self.tasks = train_tasks + eval_tasks
@@ -57,75 +118,83 @@ class ARCDataset:
             assert len(tasks) >= 1, 'Specify at least one task.'
             self.tasks = tasks
 
-        #work only with ColorPermutation
-        self.aug_tasks = None
-        if(augs is not None):
-            for aug in augs:
-                f = partial(aug_task, arc_ds=self, max_permutations=aug.max_permutations, max_colors=aug.max_colors)
-                mcp = min(mp.cpu_count(), len(tasks)) #aware case when tasks < cpu
-                with Pool(mcp) as p:
-                    insts = p.map(f, self.tasks, len(self.tasks) // mcp)
+        # populate breaks for futher navigation througout transforms
+        # each transform has `count()` method which returns #n new tasks
+        # we record #n transformations per task in `breaks` which contain intervals
+        if transforms:
+            breaks = [0]
+            for task in self.tasks:
+                per_task_cnt = 0
+                for tr in transforms:
+                    per_task_cnt += tr.count(self[task])
+                breaks.append(per_task_cnt)
+            
+            # `breaks` will contain breaks for all dataset.
+            # for example, (break[1] - break[0]) shows transformation count for first task
+            self.breaks = np.cumsum(breaks)
+            assert len(self.breaks) == len(self.tasks) + 1
 
-            breaks = [0] + list(map(len, insts))
-            breaks = np.cumsum(breaks)
-            aug_tasks = pd.DataFrame(index=pd.IntervalIndex.from_breaks(breaks, closed='left', name='intervals'), data=insts)
-            self.aug_tasks = aug_tasks.set_index([self.tasks], append=True)
-                
+        self.transforms = transforms
+
     def __repr__(self):
         n_train = len([t for t in self.tasks if 'train' in t])
         n_eval = len([t for t in self.tasks if 'evaluation' in t])
         n_test = len([t for t in self.tasks if 'test' in t])
+        n_trasformed = len(self)
 
-        return ('data = {}, train = {}, eval = {}, test = {}'
-                .format(len(self.tasks), n_train, n_eval, n_test))
+        return ('ARC folder distribution: all = {}, train = {}, eval = {}, test = {}. Transformed : {}'
+                .format(len(self.tasks), n_train, n_eval, n_test, n_trasformed))
 
     def __len__(self):
-        if self.aug_tasks is None:
+        if self.transforms:
+            return self.breaks[-1]
+        else:
             return len(self.tasks)
-        else:
-            return self.aug_tasks.index[-1][0].right
-
-    def stoi(self, taskname):
-        "Task name to index."
-        return self.tasks.index(taskname)
-
-    def itos(self, id):
-        'Index to task name'
-        if(self.aug_tasks is not None):
-            return self.aug_tasks.loc[id].index[0]
-        else:
-            return self.tasks[id]
 
     def __getitem__(self, id):
-        
-        if(type(id) is str):
-            id = self.stoi(id)
+        "Return transformed task only if `id` is `int` and transforms exist"
 
-        if(self.aug_tasks is None):
-            with open(self.tasks[id]) as f:
+        # id to task name
+        if(type(id) is int and not self.transforms):
+            id = self.tasks[id]
+
+        if(type(id) is not int): # id is str, untransformed task
+            with open(id) as f:
                 sample = json.load(f)
-
             # lists of 2-D arrays
-            train_x = list(
-                map(lambda d: np.array(d['input']),  sample['train']))
-            train_y = list(
-                map(lambda d: np.array(d['output']), sample['train']))
-            test_x = list(
-                map(lambda d: np.array(d['input']),  sample['test']))
+                train_x = list(
+                    map(lambda d: np.array(d['input']),  sample['train']))
+                train_y = list(
+                    map(lambda d: np.array(d['output']), sample['train']))
+                test_x = list(
+                    map(lambda d: np.array(d['input']),  sample['test']))
 
-            #submission case
-            test_y = None
+                #submission case
+                test_y = None
 
-            #not submission case
-            if('output' in sample['test'][0]):
-                test_y = list(map(lambda d: np.array(d['output']), sample['test']))
+                #not submission case
+                if('output' in sample['test'][0]):
+                    test_y = list(map(lambda d: np.array(d['output']), sample['test']))
 
-            return train_x, train_y, test_x, test_y
+                # (list(np.array(ndim=2)), ...)
+                return train_x, train_y, test_x, test_y
         else:
-            v = self.aug_tasks.loc[(id, slice(None)),][0]
-            aug_index = id - v.index[0][0].left
-            aug_inst = v.values[0]
-            return aug_inst[aug_index]
+            # make all permuations
+            task_idx = np.searchsorted(self.breaks, id) - 1
+            task = self.tasks[task_idx] # str
+            break_len = self.breaks[task_idx + 1] - self.breaks[task_idx]
+            original_data = self[task]
+            per_data = original_data
+
+            # get index for each transform and transform data
+            for t in self.transforms:
+                c = t.count(original_data)
+                q, r = divmod(break_len, c)
+                per_data = t.transform_data(q, per_data)
+                break_len = r
+
+            return per_data
+
 
 
     # def filter_tasks(self, filters):
@@ -191,46 +260,45 @@ class GPTDataset(Dataset):
         self.n_colors = n_colors
         self.n_context = n_context
         self.padding = padding # expand x to n_context
-        self.max_y_size = 30 * 30 + 30 + 1 # max flatten y size with special tokens(end_lines, end_episode)
+        # max flatten y size with special tokens(30 end_lines,1 end_episode)
+        self.max_y_size = 30 * 30 + 30 + 1
         
         # make special tokens 
-        self.end_line = n_colors + 0    # array of shape (10, 3) has 10 end_lines
-        self.promt = n_colors + 1       # promt after every x
-        self.end_episode = n_colors + 2 # end of episode
-        self.pad = n_colors + 3       # padding token (expand to n_context from the begining)
+        self.end_line_token = n_colors + 0    # array of shape (10, 3) has 10 end_lines
+        self.promt_token = n_colors + 1       # promt after every x
+        self.end_episode_token = n_colors + 2 # end of episode
+        self.pad_token = n_colors + 3       # padding token (expand to n_context from the begining)
         
         self.vocab_size = n_colors + 4
         
     def __len__(self):
         return len(self.ds)
     
-    def flat_all_sample(self, x, y, x_test, y_test):
-        
-        def flat_2D_field(x):
-            "Add column of `end_line` tokens and flat 2D array."
-            a = np.array([[self.end_line]] * x.shape[0], dtype=np.long)
-            a = np.hstack([x, a])
-            return a.ravel()
+    def flat_2D_field(self, x):
+        "Add column of `end_line` tokens and flat 2D array."
+        a = np.array([[self.end_line_token]] * x.shape[0], dtype=np.long)
+        a = np.hstack([x, a])
+        return a.ravel()
+
+    def flat_pair(self, pair):
+        "Flat x, y pairs and add `promt` and `end_episode` tokens"
+        x, y = pair
+        x = self.flat_2D_field(x)
+        y = self.flat_2D_field(y)
+        return np.concatenate([x, self.promt_token, y, self.end_episode_token], axis=None)
     
-        def flat_pair(pair):
-            "Flat x,y pairs and add `promt` and `end_episode` tokens"
-            x, y = pair
-            x = flat_2D_field(x)
-            y = flat_2D_field(y)
-            return np.concatenate([x, self.promt, y, self.end_episode], axis=None)
-        
-        def pad(seq, to, direct):
-            "Pad sequence to left or right."
-            x = np.array([self.pad] * to, dtype=np.long)
-            if(direct == 'left'):
-                x[-len(seq):] = seq
-            if(direct == 'right'):
-                x[:len(seq)] = seq
-            return x
-        
-        
+    def pad(self, seq, to, direct):
+        "Pad sequence to left or right."
+        x = np.array([self.pad_token] * to, dtype=np.long)
+        if(direct == 'left'):
+            x[-len(seq):] = seq
+        if(direct == 'right'):
+            x[:len(seq)] = seq
+        return x
+
+    def flat_all_sample(self, x, y, x_test, y_test):
         # flat train pairs
-        xy = list(map(flat_pair, zip(x, y)))
+        xy = list(map(self.flat_pair, zip(x, y)))
         xy = np.concatenate(xy, axis=None)
         
         #flat test pair
@@ -241,50 +309,97 @@ class GPTDataset(Dataset):
         x_test = x_test[0]
         y_test = y_test[0]
             
-        xt = flat_2D_field(x_test)
-        yt = flat_2D_field(y_test)
+        xt = self.flat_2D_field(x_test)
+        yt = self.flat_2D_field(y_test)
         
         # just add end of episode
-        y = np.concatenate([yt, self.end_episode], axis=None)
+        y = np.concatenate([yt, self.end_episode_token], axis=None)
         
         # pad y to max flattened 2D field
-        max_tokens_on_field = 30**2 + 30 + 1
-        if(len(y) < max_tokens_on_field and self.padding):
-            y = pad(y, max_tokens_on_field, 'right')
+        if(len(y) < self.max_y_size and self.padding):
+            y = self.pad(y, self.max_y_size, 'right')
         
         # context: concat all
-        x = np.concatenate([xy, xt, self.promt, y], axis=None)
+        x = np.concatenate([xy, xt, self.promt_token, y], axis=None)
         
         # padding
         if(len(x) < self.n_context and self.padding): # expand sample to n_context
-            x = pad(x, self.n_context, 'left')
+            x = self.pad(x, self.n_context, 'left')
             
         return x, y
     
     def __getitem__(self, id):
+        "Get raw example, then flat it and add special symbols."
         x, y, x_test, y_test = self.ds[id]
         x, y = self.flat_all_sample(x, y, x_test, y_test)
         return x, y
 
 
-import download
+from download import try_download_from_bucket
 import pickle
+import logging
+md_logger = logging.getLogger('MedianDataset')
 
 
 class MedianDataset():
-    def __init__(self, datadir='data/datasets/median/'):
-        files = ['test_dataset.pickle', 'train_dataset.pickle']
-        download.try_download_from_bucket(datadir, files)
-        with open(datadir + files[1], 'rb') as f:
-            self.train_dataset = pickle.load(f)
-        with open(datadir + files[1], 'rb') as f:
-            self.test_dataset = pickle.load(f)
+    # TODO do abstract class
+    # TODO api for building datasets
+    def __init__(self, download=True, datadir='./data/datasets/median/', files = ['train.pickle', 'test.pickle', 'val.pickle']):
+        from pathlib import Path
+        Path(datadir).mkdir(parents=True, exist_ok=True)
+        self.datadir = datadir
+        self.files = files
+        if download:
+            try_download_from_bucket(datadir, files)
+            self.datasets = []
+            for f in files:
+                with open(datadir + f, 'rb') as file:
+                    self.datasets.append(pickle.load(file))
+        else: self.create_new()
 
-    def get_test(self):
-        return self.test_dataset
+    def create_new(self):
+        "Find tasks with median x length. Create train, test, val ARCDatasets."
 
-    def get_train(self):
-        return self.train_dataset
+        # TODO remove hardcode
+        n_context = 2048
+        ds = LightARCDataset()
+        gpt_ds = GPTDataset(ds, 10, n_context, True)
+        lxs = []
+
+        for id in range(len(ds)):
+            x_gpt, _ =  gpt_ds[id]
+            lxs.append(len(x_gpt))
+            
+            # tests
+            #check special token counts
+            x, *_ = ds[id]
+            assert (x_gpt == gpt_ds.end_episode_token).sum() == len(x) + 1, "End of episodes missmatched."
+            assert (x_gpt == gpt_ds.promt_token).sum() == len(x) + 1, "Promts missmatched."
+        #     assert (x_gpt == no_aug_ds.new_line).sum() == len(x) + 1, "Promts missmatched."
+            
+        lxs = pd.Series(lxs)
+        md_logger.info('Median length : {}'.format(lxs.median()))
+        indices = lxs[lxs <= n_context].index.tolist()
+        median_tasks = np.array(ds.tasks)[indices].tolist()
+
+        train, test, val = .8, .1, .1
+        train = int(train * len(median_tasks))
+        test = int(test * len(median_tasks))
+        train, test, val = np.array_split(median_tasks, [train, test + train])
+
+        md_logger.info('Lengths before transforms. train : {}, test : {}, val : {}'. format(len(train), len(test), len(val)))
+
+        train = LightARCDataset(tasks=train, transforms=[LightWeightColorPermutation(max_colors=10, limit=10000)])
+        test = LightARCDataset(tasks=test)
+        val = LightARCDataset(tasks=val, transforms=[LightWeightColorPermutation(max_colors=10, limit=100)])
+
+        md_logger.info('Lengths after transforms. train : {}, test : {}, val : {}'. format(len(train), len(test), len(val)))
+        self.datasets = [train, test, val]
+
+        for i, file in enumerate(self.files):
+            with open(self.datadir + file, 'wb') as f:
+                # f.writelines(['123', '321'])
+                pickle.dump(self.datasets[i], f)
 
 ###
 #Generation
--- train.py
+++ train.py
@@ -72,16 +72,19 @@ FLAGS = args_parse.parse_common_options(
 import sys
 import download
 import logging
+import numpy as np
 from datasets import MedianDataset, GPTDataset
 import pickle
 from utils import set_seed
 import torch
 import torch.optim as optim
+from torch.utils.data import Subset
 import torch_xla.core.xla_model as xm
 import torch_xla.distributed.xla_multiprocessing as xmp
 import torch_xla.distributed.parallel_loader as pl
 import torch_xla.test.test_utils as test_utils
 from mingpt.model import GPT
+SERIAL_EXEC = xmp.MpSerialExecutor()
 
 
 # set up logging
@@ -103,51 +106,66 @@ def _train_update(device, step, loss, tracker, epoch, writer):
       summary_writer=writer)
 
 
+def get_dataset():
+        ds = MedianDataset(download=True)
+        train, test, val = [GPTDataset(d, 10, 2048, True) for d in ds.datasets]
+        return train, test, val
+
+
 def train():
     set_seed(333)
-    median_dataset = MedianDataset()
-    train_dataset = GPTDataset(median_dataset.get_train(), n_colors=10, n_context=2048, padding=True)
-    # test_dataset = GPTDataset(median_dataset.get_test(),  n_colors=10, n_context=2048, padding=True)
+
+    if not xm.is_master_ordinal():
+        xm.rendezvous('download_only_once')
+    train, test, val = SERIAL_EXEC.run(get_dataset)
+    if xm.is_master_ordinal():
+        xm.rendezvous('download_only_once')
 
     train_sampler = torch.utils.data.distributed.DistributedSampler(
-        train_dataset,
+        train,
         num_replicas=xm.xrt_world_size(),
         rank=xm.get_ordinal(),
         shuffle=True)
-  
+
+    val_sampler = torch.utils.data.distributed.DistributedSampler(
+        val,
+        num_replicas=xm.xrt_world_size(),
+        rank=xm.get_ordinal(),
+        shuffle=False)
+
     # test_sampler = torch.utils.data.distributed.DistributedSampler(
-    #     test_dataset,
+    #     test_ds,
     #     num_replicas=xm.xrt_world_size(),
     #     rank=xm.get_ordinal(),
     #     shuffle=False)
   
-    # Creates dataloaders, which load data in batches
-    # Note: test loader is not shuffled or sampled
     train_loader = torch.utils.data.DataLoader(
-        train_dataset,
+        train,
         batch_size=FLAGS.batch_size,
         sampler=train_sampler,
         num_workers=FLAGS.num_workers,
         drop_last=FLAGS.drop_last)
 
+    val_loader = torch.utils.data.DataLoader(
+        val,
+        batch_size=FLAGS.batch_size,
+        sampler=val_sampler,
+        num_workers=FLAGS.num_workers,
+        drop_last=FLAGS.drop_last)
+
     # test_loader = torch.utils.data.DataLoader(
-    #     test_dataset,
-    #     batch_size=FLAGS['batch_size'],
+    #     test_ds,
+    #     batch_size=FLAGS.batch_size,
     #     sampler=test_sampler,
-    #     shuffle=False,
-    #     num_workers=FLAGS['num_workers'],
-    #     drop_last=FLAGS['drop_last'])
+    #     num_workers=FLAGS.num_workers,
+    #     drop_last=FLAGS.drop_last)
 
-    # lr = FLAGS.lr * xm.xrt_world_size()
-
-    if (FLAGS.device):
-        device = FLAGS.device
-    else:
-        device = xm.xla_device()
-    model = GPT(FLAGS).to(device).train()
+    device = xm.xla_device()
+    model = xmp.MpModelWrapper(GPT(FLAGS)).to(device).train()
     writer = None
     if xm.is_master_ordinal():
         writer = test_utils.get_summary_writer(FLAGS.logdir)
+
     optimizer = optim.SGD(
         model.parameters(),
         lr=FLAGS.lr,
@@ -169,9 +187,23 @@ def train():
             if step % FLAGS.log_steps == 0:
                 xm.add_step_closure(
                     _train_update, args=(device, step, loss, tracker, epoch, writer))
-    
+
+    def val_loop_fn(loader):
+        model.eval()
+        tracker = xm.RateTracker()
+        losses = []
+        with torch.no_grad:
+            for step, (data, target) in enumerate(loader):
+                _, loss = model(data, target)
+                tracker.add(FLAGS.batch_size)
+                losses.append(loss.mean())
+                if step % FLAGS.log_steps == 0:
+                            xm.add_step_closure(
+                                _train_update, args=(device, step, loss, tracker, epoch, writer))
+        
+           
     train_device_loader = pl.MpDeviceLoader(train_loader, device)
-    # test_device_loader = pl.MpDeviceLoader(test_loader, device)
+    # val_device_loader = pl.MpDeviceLoader(val_loader, device)
     # accuracy, max_accuracy = 0.0, 0.0
     for epoch in range(1, FLAGS.num_epochs + 1):
         xm.master_print('Epoch {} train begin {}'.format(epoch, test_utils.now()))
