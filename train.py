20d6d520a0e11af730e2dee0e51abf43d5181512
Vitaliy Shumkov <v.shumkov.v.shumkov@gmail.com>
2020-12-27 18:40:56 +0300

    add restore logic

--- train.py
+++ train.py
@@ -21,8 +21,13 @@ import torch_xla.distributed.parallel_loader as pl
 # import torch_xla.test.test_utils as test_utils
 from mingpt.model import GPT
 import wandb
+from wandb.util import generate_id
 import hydra
-from omegaconf import DictConfig
+# from hydra.core.utils import setup_globals
+from omegaconf import DictConfig, OmegaConf
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 def get_dataset(fast_run):
@@ -36,22 +41,22 @@ def get_dataset(fast_run):
             val_trans = [ColorPermutation(max_colors=FLAGS.n_colors, limit=100)]
         ds = MaxNDataset(transforms=[train_trans, test_trans, val_trans], config=FLAGS)
         train, test, val = ds.datasets
-        logger.info("Dataset lenghts: train = {}, test = {}, val = {}".format(len(train), len(test), len(val)))
+        LOGGER.info("Dataset lenghts: train = {}, test = {}, val = {}".format(len(train), len(test), len(val)))
         return train, test, val
 
 
 def train(rank):
     def train_loop_fn(loader, val_loader, epoch):
-        model.train()
+        MODEL.train()
         tq = tqdm(loader, unit_scale=FLAGS.n_cores) if xm.is_master_ordinal(True) else loader
         loss_mean = 0.0
-        val_losses = []
+        # val_losses = []
         for step, (x, y) in enumerate(tq):
             optimizer.zero_grad()
-            _, loss = model(x, y)
+            _, loss = MODEL(x, y)
             loss = loss.mean()
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), FLAGS.grad_norm_clip)
+            torch.nn.utils.clip_grad_norm_(MODEL.parameters(), FLAGS.grad_norm_clip)
             loss_mean = (loss_mean * (step) + loss.item())/(step + 1)
             xm.optimizer_step(optimizer)
 
@@ -62,25 +67,24 @@ def train(rank):
                     tq.set_description(f"epoch {epoch}: loss: {loss_mean_mr:.3f}")
                     wandb.log({'loss_train':loss_mean_mr}, step=step*FLAGS.n_cores)
 
-            # validate
-            if not step == 0 and (step*FLAGS.n_cores) % FLAGS.val_step < FLAGS.n_cores or step == len(tq) - 1:
-                if(FLAGS.save):
-                    # save
-                    save(epoch, step*FLAGS.n_cores)
-                val_loss = val_loop_fn(val_loader)
-                val_losses.append(val_loss)
-                if(xm.is_master_ordinal()):
-                    wandb.log({'loss_val':val_loss}, step=step*FLAGS.n_cores)
-                model.train()
-        return loss_mean, val_losses
+        # validate on the end of epoch or per interval
+        # if not step == 0 and (step*FLAGS.n_cores) % FLAGS.val_step < FLAGS.n_cores or step == len(tq) - 1:
+        # if(FLAGS.save):
+            # save(epoch, step*FLAGS.n_cores)
+        val_loss = val_loop_fn(val_loader)
+        # val_losses.append(val_loss)
+        if(xm.is_master_ordinal()):
+            wandb.log({'loss_val':val_loss}, step=step*FLAGS.n_cores)
+        MODEL.train()
+        return loss_mean, val_loss
 
     def val_loop_fn(loader):
-        model.eval()
+        MODEL.eval()
         loss_mean = 0.0
         tq = tqdm(loader, leave=False, unit_scale=FLAGS.n_cores) if xm.is_master_ordinal(True) else loader
         with torch.no_grad():
             for step, (data, target) in enumerate(tq):
-                _, loss = model(data, target)
+                _, loss = MODEL(data, target)
                 loss = loss.mean()
                 loss_mean = (loss_mean * (step) + loss.item())/(step + 1)
                 if(xm.is_master_ordinal()):
@@ -89,38 +93,35 @@ def train(rank):
         return loss_mr
 
     def test_loop_fn(loader):
-        model.eval()
+        MODEL.eval()
         loss_mean = 0.0
         tq = tqdm(loader, total=int(len(loader))) if xm.is_master_ordinal(True) else loader
         with torch.no_grad():
             for step, (data, target) in enumerate(tq):
-                _, loss = model(data, target)
+                _, loss = MODEL(data, target)
                 loss = loss.mean()
                 loss_mean = (loss_mean * (step) + loss.item())/(step + 1)
                 loss_mr = xm.mesh_reduce('loss_test', loss_mean, np.mean)
         return loss_mr
     
-    def save(epoch, step):
+    def save(filename):
         # copied from xm.save()
         # FLAGS will be global after implementing distributed mode
         master_only=True
         global_master=False
         should_write_data = not master_only or xm.is_master_ordinal(local=not global_master)
         curr_state = {
-            'model_state_dict': xm._maybe_convert_to_cpu(MODEL._model.state_dict(), convert=should_write_data),
+            'model_state_dict': xm._maybe_convert_to_cpu(MODEL.state_dict(), convert=should_write_data),
             'optimizer_state_dict': xm._maybe_convert_to_cpu(optimizer.state_dict(), convert=should_write_data),
+            'flags': FLAGS._content,
         }
     
         if(xm.is_master_ordinal()):
-            name = os.path.join(wandb.run.dir, f'model_epoch{epoch}_step{step}.pt')
-            torch.save(curr_state, name)
-            wandb.save(name, base_path=wandb.run.dir)
+            torch.save(curr_state, filename)
+            wandb.save(filename)
         xm.rendezvous('save')
 
     set_seed(FLAGS.seed)
-    # global SERIAL_EXEC, MODEL
-    # global MODEL
-    # train, test, val = SERIAL_EXEC.run(lambda: get_dataset(FLAGS.fast_run))
     train, test, val = get_dataset(FLAGS.fast_run)
 
     train_sampler = torch.utils.data.distributed.DistributedSampler(
@@ -170,89 +171,125 @@ def train(rank):
         # device = xm.xla_device(devkind='CPU')
     else:
         device = xm.xla_device()
-    model = MODEL.to(device)
-    # writer = None
-    
+    MODEL.to(device)
+    optimizer = MODEL.configure_optimizers(FLAGS)
+
     if(FLAGS.scale_lr):
         FLAGS.lr *=FLAGS.n_cores
-    optimizer = model.configure_optimizers(FLAGS)
 
+    # resume
     if xm.is_master_ordinal(True):
-        wandb.init(project='test', config=FLAGS, settings=wandb.Settings(_disable_stats=True))
+        wandb.init(
+            id=HYDRA_FLAGS.job.id,
+            group=HYDRA_FLAGS.job.name,
+            project='gpt',
+            config=FLAGS._content,
+            resume='allow',
+            settings=wandb.Settings(_disable_stats=True))
 
+        if wandb.run.resumed:
+            api = wandb.Api()
+            run = api.run(f'viy/gpt/{HYDRA_FLAGS.job.id}')
+
+            # check file to restore
+            can_restore = False
+            for f in run.files():
+                if FLAGS.preempt in f.name:
+                    can_restore = True
+            
+            if(can_restore):            
+                wandb.restore(FLAGS.preempt, root='./')
+                chpt = torch.load(FLAGS.preempt)
+                MODEL.load_state_dict(chpt['model_state_dict'])
+                optimizer.load_state_dict(chpt['optimizer_state_dict'])
+                # FLAGS.update(chpt['flags'])
+                OmegaConf.update(FLAGS, 'patience', chpt['flags']['patience'])
+                OmegaConf.update(FLAGS, 'init_epoch', chpt['flags']['init_epoch'])
+                OmegaConf.update(FLAGS, 'best', chpt['flags']['best'])
+                LOGGER.info(f'Load weights from checkpoint {FLAGS.preempt}')
+    
     if(FLAGS.n_cores > 1):
         train_loader = pl.MpDeviceLoader(train_loader, device)
         val_loader = pl.MpDeviceLoader(train_loader, device)
         test_loader = pl.MpDeviceLoader(test_loader, device)
 
     xm.rendezvous('wait all')
-    for epoch in range(1, FLAGS.n_epochs + 1):
-        # xm.master_print('epoch {}: train begin {}, batch_size {}'.format(epoch, test_utils.now(), FLAGS.batch_size))
-        # train_sampler.set_epoch(epoch)
-        train_loss, val_losses = train_loop_fn(train_loader, val_loader, epoch)
+    for epoch in range(FLAGS.init_epoch, FLAGS.n_epochs + 1):
+        if xm.is_master_ordinal():
+            LOGGER.info('Start epoch {} with batch_size {} and best {}'.
+                format(epoch, FLAGS.batch_size, FLAGS.best))
+
+        train_loss, val_loss = train_loop_fn(train_loader, val_loader, epoch)
+        FLAGS.init_epoch += 1
+        if(FLAGS.best): 
+            if FLAGS.best <= val_loss:
+                FLAGS.patience += 1
+            else: 
+                FLAGS.best = val_loss
+                FLAGS.patience = 0
+                wandb.run.summary["best_loss"] = val_loss
+                wandb.run.summary["best_epoch"] = epoch
+                # save best checkpoint independently of preemptible checkpoint
+                save('best.pt')
+
+        else: FLAGS.best = val_loss
+        save('preempt.pt')
+
         test_loss = test_loop_fn(test_loader)
-        # xm.master_print('epoch {} train end {}, train: {:.2f}, val: {:.2f}, test: {:.2f}'.
-            # format(epoch, test_utils.now(), train_loss, np.mean(val_losses), test_loss))
-    # if(xm.is_master_ordinal(local=True)):
-        # wandb.clo
-        # writer.flush()
-        # writer.close()
-    
-# def add_train_args(parent_parser):
-    # TODO check add_positions 
-    # parser = ArgumentParser(parents=[parent_parser], add_help=False)
-    # parser.add_argument("--batch_size", type=int, default=8)
-    # parser.add_argument("--n_epochs", type=int, default=5)
-    # parser.add_argument("--n_cores", type=int, default=1)
-    # parser.add_argument("--n_workers", type=int, default=1)
-    # parser.add_argument("--log_steps", type=int, default=10) # log
-    # parser.add_argument("--val_steps", type=int, default=15000) # make validation every
-    # # parser.add_argument("--log_dir", type=str) # tensorboard and checkpoint dir
-    # parser.add_argument("--fast_run", action='store_true') # use fast dataset with no transformations
-    # parser.add_argument("--log_app", action='store_true') # enable logging
-    # parser.add_argument("--scale_lr", action='store_true') # mult lr by num_cores as xm.optimizer sums batches grads(see https://github.com/pytorch/xla/issues/1781#issuecomment-601849130)
-    # parser.add_argument("--debug", action="store_true") # work with power on TPU, set device to cpu
-    # parser.add_argument("--grad_norm_clip", type=float, default=1.0)
-    # parser.add_argument("--save", action='store_true')
-    # parser.add_argument("--seed", type=int, default=333)
-    # return parser
+
+        if xm.is_master_ordinal():
+            LOGGER.info('Finish epoch {}. train: {:.2f}, val: {:.2f}, test: {:.2f}, best: {:.2F}, patience: {}/{}'.
+                format(epoch, train_loss, val_loss, test_loss, FLAGS.best, FLAGS.patience, FLAGS.early_stop_patience))
+        
+        if(FLAGS.patience == FLAGS.early_stop_patience):
+            LOGGER.info('Stop training')
+            break
+                
+    if(xm.is_master_ordinal(local=True)):
+        wandb.finish()
 
 
-logger = logging.getLogger(__name__)
+def prepare_flags_for_training():
+    # global FLAGS
 
-def map_fn(rank, args):
-    global FLAGS, MODEL
-    FLAGS = args
+    # exlude validation per interval
+    if FLAGS.val_step == 0:
+        FLAGS.val_step = np.inf
+
+    # keep only dict from DictConfig
+    # FLAGS = FLAGS._content
+
+
+def map_fn(rank, *args):
+    global FLAGS, HYDRA_FLAGS, MODEL
+    FLAGS, HYDRA_FLAGS = args
+    prepare_flags_for_training()
     MODEL = GPT(FLAGS)
+    wandb.login()
     train(rank)
-
-    # Barrier to prevent master from exiting before workers connect.
     xm.rendezvous('exit')
     # sys.exit(21)
 
 @hydra.main(config_path='conf', config_name="config")
 def main(cfg: DictConfig):
-    assert os.environ['wandb'], 'Specify wandb login'
+    assert os.environ['WANDB_API_KEY'], 'Specify wandb api key'
+
     os.environ['PYTHONWARNINGS'] = 'ignore:semaphore_tracker:UserWarning'
     os.environ['XLA_USE_BF16'] = '1'
     if(cfg.debug):
         os.environ['XRT_DEVICE_MAP'] = 'CPU:0;/job:localservice/replica:0/task:0/device:XLA_CPU:0'
         os.environ['XRT_WORKERS'] = 'localservice:0;grpc://localhost:40934'
     else:
-<<<<<<< HEAD
-        # if()
-        print(os.environ['XRT_TPU_CONFIG'])
-        # os.environ['XRT_TPU_CONFIG'] = "tpu_worker;0;10.78.79.90:8470"
-    wandb.login()
-    FLAGS.__dict__.update(cfg)
-    xmp.spawn(map_fn, args=(FLAGS,), nprocs=FLAGS.n_cores, start_method='spawn')
-=======
-        assert os.environ['XRT_TPU_CONFIG'], 'No xla device'
-    wandb.login(key=os.environ['wandb'])
-    xmp.spawn(map_fn, args=(cfg,), nprocs=cfg.n_cores, start_method='spawn')
->>>>>>> 848c083... add yamls
+        assert os.environ['XRT_TPU_CONFIG'], 'Specify xla device.'
+    
+    # allow to change
+    # OmegaConf.set_struct(cfg, False)
+    full_conf = hydra.core.hydra_config.HydraConfig.get()
+
+    xmp.spawn(map_fn, args=(cfg, full_conf,), nprocs=cfg.n_cores, start_method='spawn')
 
 
 if __name__ == '__main__':
+    OmegaConf.register_resolver("wandb_id", lambda: generate_id())
     main()
 
